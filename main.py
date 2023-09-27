ac96feb18a0ac12f0f73be002be365f81a4c549e
alexey tkach <at@powerinside.com>
2023-09-26 21:58:12 -0400

    some fix

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import discord
 from discord.ext import commands
 
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f"Mama ama landed {bot.user}")
 
 @bot.command()
 async def ping(ctx):
